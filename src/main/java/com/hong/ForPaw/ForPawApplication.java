9f77a528d5c42d997bf5b2915075fd84dc6dc32b
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-20 16:55:52 +0900

    intialize

---
+++ src/main/java/com/hong/ForPaw/ForPawApplication.java
@@ -0,0 +1,13 @@
+package com.hong.ForPaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForPawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForPawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/ForPaw/ForPawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.ForPaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForPawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
