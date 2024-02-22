b11d63f592694676b9a51ecb3b858b4ec334ca8b
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-22 16:21:28 +0900

    feat: RegionCodeRepository 레포지토리 인터페이스 구현

---
+++ src/main/java/com/hong/ForPaw/repository/RegionCodeRepository.java
@@ -0,0 +1,7 @@
+package com.hong.ForPaw.repository;
+
+import com.hong.ForPaw.domain.RegionCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionCodeRepository extends JpaRepository<RegionCode, Long> {
+}
