5d3724546ae10e0c0286c7e5cee14cc7666948f4
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-22 15:18:54 +0900

    feat: Shelter 엔티티 클래스 구현

---
+++ src/main/java/com/hong/ForPaw/domain/Shelter.java
@@ -0,0 +1,32 @@
+package com.hong.ForPaw.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shelter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String careTel;
+
+    @Column
+    private String careAddr;
+
+    @Builder
+    public Shelter(Long id, String name, String careTel, String careAddr) {
+        this.id = id;
+        this.name = name;
+        this.careTel = careTel;
+        this.careAddr = careAddr;
+    }
+}
