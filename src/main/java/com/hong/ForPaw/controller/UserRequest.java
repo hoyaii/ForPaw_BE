bf7b38bd7efda12b1a63a23fd19889cb8e3a5e4c
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-26 15:26:27 +0900

    feat: UserRequest 클래스에 CheckEmailDTO 추가

--- src/main/java/com/hong/ForPaw/controller/UserRequest.java
+++ src/main/java/com/hong/ForPaw/controller/UserRequest.java
@@ -2,5 +2,9 @@ package com.hong.ForPaw.controller;
 
 public class UserRequest {
 
-    public record loginDTO(String email, String password){}
+    public record LoginDTO(String email, String password){}
+
+    public record CheckEmailDTO(String email){}
+
+
 }
