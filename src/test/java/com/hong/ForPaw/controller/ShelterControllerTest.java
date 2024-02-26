3b5ea67249aadf246789f5a70a2277f7384539ff
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-26 23:24:29 +0900

    test: 보호소 정보 불러오기 컨트룰러 단위 테스트 구현

--- src/test/java/com/hong/ForPaw/controller/ShelterControllerTest.java
+++ src/test/java/com/hong/ForPaw/controller/ShelterControllerTest.java
@@ -1,6 +1,5 @@
 package com.hong.ForPaw.controller;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,24 +9,26 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @ActiveProfiles("local")
-class RegionCodeControllerTest {
+class ShelterControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    // DB 밀고 => 지역 코드 불러온 후에 테스트 해야함
     @Test
-    void 지역코드_불러오기_성공() throws Exception {
+    void 보호소_불러오기_성공() throws Exception {
         //given
 
         //when
         ResultActions result = mvc.perform(
-                get("/api/regionCodes/import")
+                get("/api/shelters/import")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
 
