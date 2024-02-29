5a5fe13b87f54d9e11aaa34f92e540635810771f
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-02-29 15:23:05 +0900

    feat: AnimalResponse에 AnimalDetailDTO DTO 추가

--- src/main/java/com/hong/ForPaw/controller/DTO/AnimalResponse.java
+++ src/main/java/com/hong/ForPaw/controller/DTO/AnimalResponse.java
@@ -1,21 +1,19 @@
 package com.hong.ForPaw.controller.DTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AnimalResponse {
 
     public record FindAllAnimalsDTO(List<AnimalDTO> animalDTOS){ }
 
-    public record AnimalDTO(Long id,
-                            String name,
-                            String age,
-                            String gender,
-                            String specialMark,
-                            String region,
-                            Integer inquiryNum,
-                            Integer likeNum,
-                            Boolean isLike,
+    public record AnimalDTO(Long id, String name, String age,
+                            String gender, String specialMark, String region,
+                            Integer inquiryNum, Integer likeNum, Boolean isLike,
                             String profileURL
     ){ };
 
+    public record AnimalDetailDTO(Long id, String happenPlace, String kind, String color,
+                                  String weight, LocalDate noticeSdt, LocalDate noticeEdt,
+                                  String processState, String neuter){}
 }
