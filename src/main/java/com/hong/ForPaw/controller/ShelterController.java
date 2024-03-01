80f68e2f88d33fa3facac175973b9012de218fba
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-01 12:06:10 +0900

    feat: ShelterController에 deleteZeroShelter() 메서드 구현

--- src/main/java/com/hong/ForPaw/controller/ShelterController.java
+++ src/main/java/com/hong/ForPaw/controller/ShelterController.java
@@ -1,16 +1,15 @@
 package com.hong.ForPaw.controller;
 
 import com.hong.ForPaw.controller.DTO.ShelterResponse;
+import com.hong.ForPaw.core.security.CustomUserDetails;
 import com.hong.ForPaw.core.utils.ApiUtils;
 import com.hong.ForPaw.service.ShelterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -33,5 +32,12 @@ public class ShelterController {
 
         ShelterResponse.FindAllSheltersDTO responseDTO = shelterService.findAllShelters(pageable);
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.CREATED, responseDTO));
+    }
+
+    @DeleteMapping("/shelters")
+    public ResponseEntity<?> deleteZeroShelter(@AuthenticationPrincipal CustomUserDetails userDetails){
+
+        shelterService.deleteZeroShelter(userDetails.getUser().getId());
+        return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, null));
     }
 }
