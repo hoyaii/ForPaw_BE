e152a091af9272513f91842e7cdc11d68a62890e
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-01 16:04:42 +0900

    feat: GroupUserRepository 클래스 생성

--- src/main/java/com/hong/ForPaw/repository/GroupRepository.java
+++ src/main/java/com/hong/ForPaw/repository/GroupRepository.java
@@ -2,9 +2,11 @@ package com.hong.ForPaw.repository;
 
 import com.hong.ForPaw.domain.Group.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
     Optional<Group> findByName(String name);
