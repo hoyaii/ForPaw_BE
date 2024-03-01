df2d844dc1cce42823a5f320a00825111f08ccbd
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-01 11:58:38 +0900

    feat: ShelterRepository에 동물 수가 0인 보호소를 삭제하는 deleteZeroShelter() 메서드 구현

--- src/main/java/com/hong/ForPaw/repository/ShelterRepository.java
+++ src/main/java/com/hong/ForPaw/repository/ShelterRepository.java
@@ -2,8 +2,17 @@ package com.hong.ForPaw.repository;
 
 import com.hong.ForPaw.domain.Shelter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
+
+    @Modifying
+    @Query("DELETE FROM Shelter s WHERE s.animalCnt = 0")
+    void deleteZeroShelter();
 }
