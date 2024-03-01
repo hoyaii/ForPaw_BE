d2e4b1640b0c1499fb82e4f719ece22293c607b7
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-01 15:25:09 +0900

    feat: Group 엔티티 클래스 생성

---
+++ src/main/java/com/hong/ForPaw/domain/Group.java
@@ -0,0 +1,50 @@
+package com.hong.ForPaw.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String region;
+
+    @Column
+    private String subRegion;
+
+    @Column
+    private String description;
+
+    @Column
+    private String category;
+
+    @Column
+    private String profileURL;
+
+    @Column
+    private Integer likeNum = 0;
+
+    @Builder
+    public Group(String name, String region, String subRegion, String description, String category, String profileURL, Integer likeNum) {
+        this.name = name;
+        this.region = region;
+        this.subRegion = subRegion;
+        this.description = description;
+        this.category = category;
+        this.profileURL = profileURL;
+        this.likeNum = likeNum;
+    }
+}
