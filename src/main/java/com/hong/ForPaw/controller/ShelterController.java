2ff083bbb88c5e8fdbaa323639ea917654d6e096
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-01 10:55:19 +0900

    feat: ShelterController에 findAllShelters() 메서드 구현

--- src/main/java/com/hong/ForPaw/controller/ShelterController.java
+++ src/main/java/com/hong/ForPaw/controller/ShelterController.java
@@ -1,11 +1,14 @@
 package com.hong.ForPaw.controller;
 
+import com.hong.ForPaw.controller.DTO.ShelterResponse;
 import com.hong.ForPaw.core.utils.ApiUtils;
 import com.hong.ForPaw.service.ShelterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,12 @@ public class ShelterController {
 
         shelterService.loadShelterData();
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, null));
+    }
+
+    @GetMapping("/shelters")
+    public ResponseEntity<?> findAllShelters(Pageable pageable){
+
+        ShelterResponse.FindAllSheltersDTO responseDTO = shelterService.findAllShelters(pageable);
+        return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.CREATED, responseDTO));
     }
 }
