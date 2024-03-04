b6717c94043893b9c77e960317b6150c3e3cbac6
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-04 13:31:47 +0900

    feat: PostController에 findPostList() 메서드 구현

--- src/main/java/com/hong/ForPaw/controller/PostController.java
+++ src/main/java/com/hong/ForPaw/controller/PostController.java
@@ -1,18 +1,18 @@
 package com.hong.ForPaw.controller;
 
 import com.hong.ForPaw.controller.DTO.PostRequest;
+import com.hong.ForPaw.controller.DTO.PostResponse;
 import com.hong.ForPaw.core.security.CustomUserDetails;
 import com.hong.ForPaw.core.utils.ApiUtils;
+import com.hong.ForPaw.domain.Post.Type;
 import com.hong.ForPaw.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,5 +26,12 @@ public class PostController {
 
         postService.createPost(requestDTO, userDetails.getUser().getId());
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, null));
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<?> findPostList(@RequestParam Type type, Pageable pageable){
+
+        PostResponse.FindPostListDTO responseDTO = postService.findPostList(type, pageable);
+        return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, responseDTO));
     }
 }
