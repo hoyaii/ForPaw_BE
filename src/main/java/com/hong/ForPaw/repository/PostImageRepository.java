9f2cf290a44ea7e3212fc89b50c8615c16826914
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-04 12:05:46 +0900

    feat: PostImageRepository 레포지토리 인터페이스 생성

---
+++ src/main/java/com/hong/ForPaw/repository/PostImageRepository.java
@@ -0,0 +1,10 @@
+package com.hong.ForPaw.repository;
+
+import com.hong.ForPaw.domain.Post.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+    
+}
