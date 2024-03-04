9cc070aae55518e3636e46195c65e5eb22ca6144
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-04 21:09:37 +0900

    feat: PostLikeRepository 레포지토리 인터페이스 생성

---
+++ src/main/java/com/hong/ForPaw/repository/PostLikeRepository.java
@@ -0,0 +1,10 @@
+package com.hong.ForPaw.repository;
+
+import com.hong.ForPaw.domain.Post.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    
+}
