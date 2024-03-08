9ec618af714ab35318ff43c67bc85bd95f982dee
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-08 14:07:12 +0900

    feat: AlarmService에 send() 메서드 구현

--- src/main/java/com/hong/ForPaw/controller/DTO/AlarmResponse.java
+++ src/main/java/com/hong/ForPaw/controller/DTO/AlarmResponse.java
@@ -1,10 +1,12 @@
 package com.hong.ForPaw.controller.DTO;
 
+import com.hong.ForPaw.domain.Alarm.Type;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class AlarmResponse {
 
     public record FindAlarmsDTO(List<AlarmDTO> alarms) {}
 
-    public record AlarmDTO(Long id, String content, boolean isRead) {}
+    public record AlarmDTO(Long id, String name, String content, LocalDateTime createdAt) {}
 }
--- src/main/java/com/hong/ForPaw/service/AlarmService.java
+++ src/main/java/com/hong/ForPaw/service/AlarmService.java
@@ -1,5 +1,9 @@
 package com.hong.ForPaw.service;
 
+import com.hong.ForPaw.controller.DTO.AlarmResponse;
+import com.hong.ForPaw.domain.Alarm.Alarm;
+import com.hong.ForPaw.domain.Alarm.Type;
+import com.hong.ForPaw.domain.User.User;
 import com.hong.ForPaw.repository.AlarmRepository;
 import com.hong.ForPaw.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +25,7 @@ public class AlarmService {
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
     @Transactional
-    public SseEmitter connectToAlarm(String userId, String lastEventId) {
+    public SseEmitter connectToAlarm(String userId, String lastEventId) { // 여기서 userId는 서버에 연결된 클라이언트의 id
         // SseEmitter 객체 생성
         String emitterId = generateIdByTime(userId);
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
@@ -42,6 +46,28 @@ public class AlarmService {
         return emitter;
     }
 
+    @Transactional
+    public void send(User receiver, Type alarmType, String content, String redirectURL) {
+        // 알림 생성 및 저장
+        Alarm alarm = alarmRepository.save(createAlarm(receiver, alarmType, content, redirectURL));
+
+        // 이벤트 ID 생성
+        String receiverId = receiver.getId().toString();
+        String eventId = generateIdByTime(receiverId);
+
+        // SSE Emitter 조회 => 사용자가 여러 기기에서 접속하여 여러 Emitter를 생성했더라도, 모든 Emitter를 찾아 알림을 전송
+        Map<String, SseEmitter> emitters = emitterRepository.findAllEmitterStartWithMemberId(receiverId);
+
+        // 각 Emitter로 알림 전송
+        emitters.forEach(
+                (key, emitter) -> {
+                    // 클라이언트가 서버에 연결된 후 발생한 알림은 캐시에 저장되어, 클라이언트가 다시 연결할 때 놓친 알림을 제공할 수 있음
+                    emitterRepository.saveEventCache(key, alarm);
+                    sendNotification(emitter, eventId, key, new AlarmResponse.AlarmDTO(alarm.getId(), alarm.getReceiver().getNickName(), alarm.getContent(), alarm.getCreatedDate()));
+                }
+        );
+    }
+
     private void sendNotification(SseEmitter emitter, String eventId, String emitterId, Object data) {
         // SseEmitter 객체를 통해 클라이언트에게 알람 이벤트를 전송
         try {
@@ -55,11 +81,13 @@ public class AlarmService {
         }
     }
 
-    private void sendMissingAlarm(String lastEventId, String userEmail, String emitterId, SseEmitter emitter) {
-        // 클라이언트가 미수신한 이벤트 목록을 전송
-        Map<String, Object> eventCaches = emitterRepository.findAllEventCacheStartWithMemberId(String.valueOf(userEmail));
+    private void sendMissingAlarm(String lastEventId, String userId, String emitterId, SseEmitter emitter) {
+        // 이벤트 캐시 조회
+        Map<String, Object> eventCaches = emitterRepository.findAllEventCacheStartWithMemberId(String.valueOf(userId));
+
+        // 누락된 알람이 있는지 캐시를 뒤져서, 누락된 알람은 다시 전송
         eventCaches.entrySet().stream()
-                .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
+                .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0) // 결과가 음수면, lastEventId가 더 작다는 의미고, 누락된 알람이라는 뜻!
                 .forEach(entry -> sendNotification(emitter, entry.getKey(), emitterId, entry.getValue()));
     }
 
@@ -67,8 +95,16 @@ public class AlarmService {
         return !lastEventId.isEmpty();
     }
 
-    private String generateIdByTime(String email) {
-        return email + "_" + System.currentTimeMillis();
+    private String generateIdByTime(String userId) {
+        return userId + "_" + System.currentTimeMillis();
     }
 
+    private Alarm createAlarm(User receiver, Type alarmType, String content, String redirectURL){
+        return Alarm.builder()
+                .receiver(receiver)
+                .content(content)
+                .redirectURL(redirectURL)
+                .type(alarmType)
+                .build();
+    }
 }
