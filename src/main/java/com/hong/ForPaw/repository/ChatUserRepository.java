5b62fb4a004fac3b47977e1e6e7c715e90f3ba60
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-17 20:29:16 +0900

    feat: ChatUserRepository 레포지토리 인터페이스 구현

--- src/main/java/com/hong/ForPaw/repository/ChatUserRepository.java
+++ src/main/java/com/hong/ForPaw/repository/ChatUserRepository.java
@@ -1,10 +1,10 @@
 package com.hong.ForPaw.repository;
 
-import com.hong.ForPaw.domain.Chat.Message;
+import com.hong.ForPaw.domain.Chat.ChatUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-
+public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
+    
 }
