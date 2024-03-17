93ee90aaefa258aa3488c9b02c67c726f124250f
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-17 20:25:01 +0900

    feat: Message 엔티티 클래스 구현

---
+++ src/main/java/com/hong/ForPaw/domain/Chat/Message.java
@@ -0,0 +1,33 @@
+package com.hong.ForPaw.domain.Chat;
+
+import com.hong.ForPaw.domain.User.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatRoom_id")
+    private ChatRoom chatRoom;
+
+    private String content;
+
+    public Message(User user, ChatRoom chatRoom, String content) {
+        this.user = user;
+        this.chatRoom = chatRoom;
+        this.content = content;
+    }
+}
