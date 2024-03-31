ed0d43ad02e2dc633897db45d035f1b1d68baa5e
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-03-31 21:17:20 +0900

    feat: S3Service에 PresignedURL 발급을 해주는 generatePresignedUrl() 메서드 구현

---
+++ src/main/java/com/hong/ForPaw/service/S3Service.java
@@ -0,0 +1,49 @@
+package com.hong.ForPaw.service;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public URL generatePresignedUrl(Long userId, HttpMethod httpMethod) {
+        // 만료 시간은 5분
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 5;
+        expiration.setTime(expTimeMillis);
+
+        // 현재 시간, userId를 기반으로 키 생성
+        String objectKey = createKey(userId);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, objectKey)
+                        .withMethod(httpMethod)
+                        .withExpiration(expiration);
+        return amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+    }
+
+    private String createKey(Long userId) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String formattedDateTime = LocalDateTime.now().format(formatter);
+        String uuid = UUID.randomUUID().toString();
+
+        return userId + "/" + formattedDateTime + "-" + uuid;
+    }
+}
