14fc3e18bfd833b32dab50d1ceff35ece50633cb
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-05-02 14:23:00 +0900

    feat: CustomerInquiry  엔티티 클래스 구현

---
+++ src/main/java/com/hong/ForPaw/domain/Inquiry/CustomerInquiry.java
@@ -0,0 +1,36 @@
+package com.hong.ForPaw.domain.Inquiry;
+
+import com.hong.ForPaw.domain.User.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "inquiry_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInquiry {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String contactMail;
+
+    public UserInquiry(User user, String title, String description, String contactMail) {
+        this.user = user;
+        this.title = title;
+        this.description = description;
+        this.contactMail = contactMail;
+    }
+}
