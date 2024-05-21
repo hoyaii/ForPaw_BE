86ad0d736e72eb5fa67b7603f84139d97db3b8dd
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-05-21 20:01:06 +0900

    feat: 어플리케이션 시작시 특정 Super Admin를 저장하는 로직 추가

--- src/main/java/com/hong/ForPaw/ForPawApplication.java
+++ src/main/java/com/hong/ForPaw/ForPawApplication.java
@@ -5,6 +5,7 @@ import com.hong.ForPaw.domain.Province;
 import com.hong.ForPaw.domain.User.UserRole;
 import com.hong.ForPaw.domain.User.User;
 import com.hong.ForPaw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +15,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class ForPawApplication {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForPawApplication.class, args);
@@ -25,20 +31,21 @@ public class ForPawApplication {
 
 	@Profile("local")
 	@Bean
-	CommandLineRunner localServerStart(PasswordEncoder passwordEncoder, UserRepository userRepository){
+	CommandLineRunner localServerStart(UserRepository userRepository){
 		return args -> {
-			//userRepository.saveAll(Arrays.asList(
-			//		newUser("yg04076@naver.com", "한홍", "호얘이", passwordEncoder, Role.USER, "www.s3.com", "대구", "수성구")
-			//));
+			// Super Admin 추가
+			if(!userRepository.existsByEmailWithRemoved("admin@naver.com")){
+				userRepository.save(newUser("admin@naver.com", "운영자", "운영자", "pnu1234!", UserRole.SUPER, "www.s3.com", Province.DAEGU, District.SUSEONG, "두산동"));
+			}
 		};
 	}
 
-	private User newUser(String email, String name, String nickName, PasswordEncoder passwordEncoder, UserRole userRole, String profileURL, Province province, District district, String subDistrict, String subRegin) {
+	private User newUser(String email, String name, String nickName, String password, UserRole userRole, String profileURL, Province province, District district, String subDistrict) {
 		return User.builder()
 				.email(email)
 				.name(name)
 				.nickName(nickName)
-				.password(passwordEncoder.encode("hong1234"))
+				.password(passwordEncoder.encode(password))
 				.role(userRole)
 				.profileURL(profileURL)
 				.province(province)
