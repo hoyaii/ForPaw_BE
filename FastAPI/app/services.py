797742c2d91497d6ffc37c72463a33ef43a6bf82
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-06-27 16:56:59 +0900

    fix: PCA를 사용해서 milvus 백터의 차원 조정 로직 추가하고, fastAPI를 시작할 때 milvus의 기존 데이터는 삭제하도록 수정

--- FastAPI/app/services.py
+++ FastAPI/app/services.py
@@ -3,10 +3,11 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
 from fastapi import HTTPException
 from contextlib import asynccontextmanager
 from .models import Animal
-from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
 import numpy as np  
 import redis
 
@@ -14,21 +15,29 @@ import redis
 def initialize_milvus():
     connections.connect("default", host="milvus-standalone", port="19530")
 
+    # 컬렉션 이름은 animal_collection
+    collection_name = "animal_collection"
+
+    # fastAPI를 시작할 때, 기존에 저장된 데이터는 삭제
+    if utility.has_collection(collection_name):
+        collection = Collection(name=collection_name)
+        collection.drop()
+
     # ID 필드와 벡터 필드 정의. 차원은 512 
     fields = [
         FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=False), 
         FieldSchema(name="vector", dtype=DataType.FLOAT_VECTOR, dim=512)  
     ]
 
-    # animal_collection이라는 이름의 컬렉션 생성
+    # 컬렉션 생성
     animal_schema = CollectionSchema(fields, "animal_vectors")
-    animal_collection = Collection("animal_collection", animal_schema)
+    animal_collection = Collection(collection_name, animal_schema)
     
     return animal_collection
 
 # MySQL 초기화 함수
 def initialize_mysql():
-    DATABASE_URL = "mysql+aiomysql://hoyai:gk011014@forpaw.cjgwcqgyck73.ap-northeast-2.rds.amazonaws.com:3306/forpaw?serverTimezone=UTC"
+    DATABASE_URL = "mysql+aiomysql://hoyai:gk011014@forpaw.cjgwcqgyck73.ap-northeast-2.rds.amazonaws.com:3306/forpaw"
     engine = create_async_engine(DATABASE_URL)
     AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
     
@@ -41,7 +50,9 @@ def initialize_redis():
 # Milvus, MySQL, 백터화 객체 초기화
 animal_collection = initialize_milvus()
 AsyncSessionLocal = initialize_mysql()
+redis_client = initialize_redis()
 vectorizer = TfidfVectorizer()
+vector_dim = 512
 
 @asynccontextmanager
 async def get_db_session():
@@ -62,13 +73,24 @@ async def load_and_vectorize_data():
     tfidf_matrix = vectorizer.fit_transform(texts)
     vectors = tfidf_matrix.toarray()
 
+    # PCA를 사용하여 벡터 차원을 512로 축소
+    pca = PCA(n_components=vector_dim)
+    reduced_vectors = pca.fit_transform(vectors)
+
     # 동물 ID 리스트 생성
     ids = [animal.id for animal in animals]
 
     # Milvus에 데이터 삽입
-    animal_collection.insert([ids, vectors])
+    animal_collection.insert([ids, reduced_vectors])
+
+    # 인덱스 생성
+    index_params = {"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}}
+    animal_collection.create_index("vector", index_params)
+
+    # 컬렉션 로드
+    animal_collection.load()
     
-    return vectors, {animal.id: idx for idx, animal in enumerate(animals)}
+    return reduced_vectors, {animal.id: idx for idx, animal in enumerate(animals)}
 
 async def get_similar_animals(animal_id, animal_index, tfidf_matrix, num_results=5):
     async with get_db_session() as db:
@@ -84,11 +106,11 @@ async def get_similar_animals(animal_id, animal_index, tfidf_matrix, num_results
             raise HTTPException(status_code=404, detail="해당 동물에 대한 인덱스가 존재하지 않습니다.")
     
         # 해당 동물의 벡터를 배열로 변환하고, 전체 데이터베이스 벡터와의 코사인 유사도 계산
-        query_vec = tfidf_matrix[idx].toarray().tolist()
+        query_vec = tfidf_matrix[idx].tolist() 
         search_params = {"metric_type": "L2", "params": {"nprobe": 10}}
         results = animal_collection.search([query_vec], "vector", search_params, limit=num_results)
         
-        similar_animal_ids = [res.id for res in results[0].ids]
+        similar_animal_ids = [res for res in results[0].ids]
 
         return similar_animal_ids
     
@@ -114,16 +136,25 @@ async def update_new_animals(animal_index, tfidf_matrix):
 
     # 새로운 텍스트 데이터를 TF-IDF 벡터로 변환
     new_vectors = vectorizer.transform(new_texts).toarray()
+    pca = PCA(n_components=vector_dim)
+    reduced_new_vectors = pca.fit_transform(new_vectors)
 
     # 새로운 동물들의 ID 리스트를 생성
     new_ids = [animal.id for animal in new_animals]
 
     # Milvus에 새로운 데이터 삽입
-    animal_collection.insert([new_ids, new_vectors])
+    animal_collection.insert([new_ids, reduced_new_vectors])
+
+    # 인덱스 생성
+    index_params = {"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}}
+    animal_collection.create_index("vector", index_params)
+
+    # 컬렉션 로드
+    animal_collection.load()
     
     # 기존 tfidf_matrix에 새로운 벡터 추가
     current_length = len(tfidf_matrix)
-    tfidf_matrix = np.vstack([tfidf_matrix, new_vectors])
+    tfidf_matrix = np.vstack([tfidf_matrix, reduced_new_vectors])
 
     # animal_index를 업데이트하여 새로운 동물들의 ID와 인덱스를 추가
     animal_index.update({animal.id: current_length + idx for idx, animal in enumerate(new_animals)})
