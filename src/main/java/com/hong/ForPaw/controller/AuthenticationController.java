e47a0ae35cf30bef480a1b7670706c66db176cc1
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-07-23 12:51:07 +0900

    feat: AuthenticationController 클래스에 findSupportList() 메서드 구현

--- src/main/java/com/hong/ForPaw/controller/AuthenticationController.java
+++ src/main/java/com/hong/ForPaw/controller/AuthenticationController.java
@@ -2,13 +2,12 @@ package com.hong.ForPaw.controller;
 
 import com.hong.ForPaw.controller.DTO.AuthenticationRequest;
 import com.hong.ForPaw.controller.DTO.AuthenticationResponse;
-import com.hong.ForPaw.controller.DTO.AuthenticationResponse.ApplyDTO;
 import com.hong.ForPaw.core.security.CustomUserDetails;
 import com.hong.ForPaw.core.utils.ApiUtils;
 import com.hong.ForPaw.domain.Apply.ApplyStatus;
+import com.hong.ForPaw.domain.Inquiry.InquiryStatus;
 import com.hong.ForPaw.domain.Report.ReportStatus;
 import com.hong.ForPaw.service.AuthenticationService;
-import java.util.List;
 
 import com.hong.ForPaw.service.UserService;
 import jakarta.validation.Valid;
@@ -65,8 +64,8 @@ public class AuthenticationController {
     }
 
     @GetMapping("/admin/adoption")
-    public ResponseEntity<?> findApplyList(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam(required = false) ApplyStatus applyStatus, @RequestParam int page){
-        AuthenticationResponse.FindApplyListDTO responseDTO = authenticationService.findApplyList(userDetails.getUser().getId(), applyStatus, page);
+    public ResponseEntity<?> findApplyList(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam(required = false) ApplyStatus status, @RequestParam int page){
+        AuthenticationResponse.FindApplyListDTO responseDTO = authenticationService.findApplyList(userDetails.getUser().getId(), status, page);
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, responseDTO));
     }
 
@@ -77,8 +76,8 @@ public class AuthenticationController {
     }
 
     @GetMapping("/admin/reports")
-    public ResponseEntity<?> findReportList(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam(required = false) ReportStatus reportStatus, @RequestParam int page){
-        AuthenticationResponse.FindReportListDTO responseDTO = authenticationService.findReportList(userDetails.getUser().getId(), reportStatus, page);
+    public ResponseEntity<?> findReportList(@RequestParam(required = false) ReportStatus status, @RequestParam int page, @AuthenticationPrincipal CustomUserDetails userDetails){
+        AuthenticationResponse.FindReportListDTO responseDTO = authenticationService.findReportList(userDetails.getUser().getId(), status, page);
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, responseDTO));
     }
 
@@ -86,5 +85,11 @@ public class AuthenticationController {
     public ResponseEntity<?> processReport(@RequestBody @Valid AuthenticationRequest.ProcessReportDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
         authenticationService.processReport(requestDTO, userDetails.getUser().getId());
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, null));
+    }
+
+    @GetMapping("/admin/supports")
+    public ResponseEntity<?> findSupportList(@RequestParam(required = false) InquiryStatus status, @RequestParam int page, @AuthenticationPrincipal CustomUserDetails userDetails){
+        AuthenticationResponse.FindSupportListDTO responseDTO = authenticationService.findSupportList(userDetails.getUser().getId(), status, page);
+        return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, responseDTO));
     }
 }
