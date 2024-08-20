4c27f6579be3419fac8b27e23fef49dac7f85d6b
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-08-20 13:44:13 +0900

    feat: 링크의 메타 데이터를 추출하기 위해 MetaDataUtils 클래스 구현

---
+++ src/main/java/com/hong/ForPaw/controller/DTO/MetaDataDTO.java
@@ -0,0 +1,4 @@
+package com.hong.ForPaw.controller.DTO;
+
+public record MetaDataDTO(String title, String description, String image, String url) {
+}
---
+++ src/main/java/com/hong/ForPaw/core/utils/MetaDataUtils.java
@@ -0,0 +1,46 @@
+package com.hong.ForPaw.core.utils;
+
+import com.hong.ForPaw.controller.DTO.MetaDataDTO;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.springframework.web.client.RestTemplate;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MetaDataUtils {
+
+    public MetaDataDTO fetchMetadata(String url) throws IOException {
+        // RestTemplate을 사용하여 웹 페이지의 HTML을 가져옴
+        RestTemplate restTemplate = new RestTemplate();
+        String html = restTemplate.getForObject(url, String.class);
+
+        // JSoup을 사용하여 HTML 파싱
+        Document doc = Jsoup.parse(Objects.requireNonNull(html));
+
+        // 메타데이터 추출
+        String title = getMetaTagContent(doc, "og:title", "title");
+        String description = getMetaTagContent(doc, "og:description", "meta[name=description]");
+        String image = getMetaTagContent(doc, "og:image", null);
+        String ogUrl = getMetaTagContent(doc, "og:url", null);
+
+        return new MetaDataDTO(title, description, image, ogUrl != null ? ogUrl : url);
+    }
+
+    private String getMetaTagContent(Document doc, String ogTag, String fallbackTag) {
+        Element metaTag = doc.selectFirst("meta[property=" + ogTag + "]");
+        if (metaTag != null) {
+            return metaTag.attr("content"); // 메타 태그의 'content' 속성 값을 반환
+        }
+
+        // 만약 Open Graph 태그가 없고, fallbackTag가 지정되어 있다면 해당 태그를 검색
+        if (fallbackTag != null) {
+            Element fallbackMetaTag = doc.selectFirst(fallbackTag);
+            if (fallbackMetaTag != null) {
+                return fallbackMetaTag.text();
+            }
+        }
+        return null;
+    }
+}
