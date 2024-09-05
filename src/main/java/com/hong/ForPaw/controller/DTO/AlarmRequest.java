8d3e10d8ef2cbdd01973dd71bb95ffd68167ee7b
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-09-05 22:21:29 +0900

    fix: AlarmRequest의 ReadAlarmDTO의 Validation 옵션 수정

--- src/main/java/com/hong/ForPaw/controller/DTO/AlarmRequest.java
+++ src/main/java/com/hong/ForPaw/controller/DTO/AlarmRequest.java
@@ -9,12 +9,13 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.hong.ForPaw.domain.Alarm.AlarmType;
 import com.hong.ForPaw.domain.User.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public class AlarmRequest {
 
-    public record ReadAlarmDTO(@NotBlank Long id) {}
+    public record ReadAlarmDTO(@NotNull Long id) {}
 
     public record AlarmDTO(Long receiverId,
                            String content,
