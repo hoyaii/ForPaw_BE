39346064005a9b3497c9051f1c768a3fe7b003c9
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-11-30 14:29:33 +0900

    fix: MeetingUserRepository의 findMeetingUsersByGroupId()의 쿼리문 수정

--- src/main/java/com/hong/forapw/core/utils/ApiUtils.java
+++ src/main/java/com/hong/forapw/core/utils/ApiUtils.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 
 public class ApiUtils {
 
+    private ApiUtils() {
+    }
+
     public static <T> ApiResult<T> success(HttpStatus httpStatus, T result) {
         return new ApiResult<>(true, httpStatus.value(), httpStatus.getReasonPhrase(), result);
     }
 
     public static ApiResult<?> error(String message, HttpStatus httpStatus) {
+
         return new ApiResult<>(false, httpStatus.value(), message, null);
     }
 
